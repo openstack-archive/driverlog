45932692224bef0aa7d4f717d6976334112bba8f
Ilya Shakhat <ishakhat@mirantis.com>
2014-05-12 18:27:19 +0400

    Refactored review processing and tests
    Part of bug 1317554
    
    Change-Id: I8edfd326e016b98a376e3a803ea70d7a8fe06812

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+
 from driverlog.processor import main
 
 import testtools
@@ -22,7 +24,22 @@ class TestMain(testtools.TestCase):
     def setUp(self):
         super(TestMain, self).setUp()
 
-    def test_process_reviews_ci_vote(self):
+        with open('tests/unit/test_data/sample_review.json') as fd:
+            self.review = json.load(fd)
+
+        with open('tests/unit/test_data/sample_default_data.json') as fd:
+            self.default_data = json.load(fd)
+
+    def test_build_ci_map(self):
+        ci_map = main.build_ci_map(self.default_data['drivers'])
+        self.assertTrue('arista-test' in ci_map)
+        self.assertEqual([{
+            'vendor': 'Arista',
+            'driver_name': 'Arista Neutron ML2 Driver'
+        }], ci_map['arista-test'])
+
+    def test_process_reviews_ci_vote_no_comment(self):
+        # check that vote is processed even if there are no comment
         review = {
             "project": "openstack/neutron",
             "branch": "master",
@@ -42,7 +59,7 @@ class TestMain(testtools.TestCase):
                     }]}
         }
 
-        ci_ids_map = {'arista-test': [('Arista', 'Arista Neutron ML2 Driver')]}
+        ci_ids_map = main.build_ci_map(self.default_data['drivers'])
         records = list(main.process_reviews(
             [review], ci_ids_map, 'openstack/neutron'))
 
@@ -62,40 +79,11 @@ class TestMain(testtools.TestCase):
         self.assertEqual(expected_record, records[0])
 
     def test_process_reviews_ci_vote_and_comment(self):
-        review = {
-            "project": "openstack/neutron",
-            "branch": "master",
-            "url": "https://review.openstack.org/92468",
-            "comments": [
-                {
-                    "timestamp": 1399411839,
-                    "reviewer": {
-                        "name": "Arista Testing",
-                        "email": "arista-openstack-test@aristanetworks.com",
-                        "username": "arista-test"
-                    },
-                    "message": "Patch Set 2: Verified+1\n\n"
-                               "Arista third party testing PASSED"
-                },
-            ],
-            "currentPatchSet": {
-                "number": "2",
-                "approvals": [
-                    {
-                        "type": "Verified",
-                        "description": "Verified",
-                        "value": "1",
-                        "grantedOn": 1399478047,
-                        "by": {
-                            "name": "Arista Testing",
-                            "username": "arista-test"
-                        }
-                    }]}
-        }
+        # check that vote and matching comment are found
 
-        ci_ids_map = {'arista-test': [('Arista', 'Arista Neutron ML2 Driver')]}
+        ci_ids_map = main.build_ci_map(self.default_data['drivers'])
         records = list(main.process_reviews(
-            [review], ci_ids_map, 'openstack/neutron'))
+            [self.review], ci_ids_map, 'openstack/neutron'))
 
         self.assertEqual(1, len(records), 'One record is expected')
 
@@ -104,7 +92,8 @@ class TestMain(testtools.TestCase):
                 'os_versions_map': {
                     'master': {
                         'comment': 'Verified+1\n\n'
-                                   'Arista third party testing PASSED',
+                                   'Arista third party testing PASSED '
+                                   '[ https://arista.box.com/s/x8z0 ]',
                         'timestamp': 1399478047,
                         'review_url': 'https://review.openstack.org/92468',
                     }
